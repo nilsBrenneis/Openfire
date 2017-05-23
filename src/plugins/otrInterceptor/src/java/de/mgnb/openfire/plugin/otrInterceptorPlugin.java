9d635dbef30c7edf3bd0059f26bb206ff8399ec0
nilsBrenneis <nilsstnurfuersam@gmail.com>
2017-05-23 12:30:27 +0200

    add interceptor

--- src/plugins/otrInterceptor/src/java/de/mgnb/openfire/plugin/otrInterceptorPlugin.java
+++ src/plugins/otrInterceptor/src/java/de/mgnb/openfire/plugin/otrInterceptorPlugin.java
@@ -1,20 +1,41 @@
 package de.mgnb.openfire.plugin;
 
+import org.jivesoftware.openfire.MessageRouter;
+import org.jivesoftware.openfire.XMPPServer;
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
+import org.jivesoftware.openfire.interceptor.InterceptorManager;
+import org.jivesoftware.openfire.interceptor.PacketInterceptor;
+import org.jivesoftware.openfire.interceptor.PacketRejectedException;
+import org.jivesoftware.openfire.session.Session;
+import org.xmpp.packet.Packet;
 
 import java.io.File;
 
 
-public class otrInterceptorPlugin implements Plugin {
+public class otrInterceptorPlugin implements Plugin, PacketInterceptor {
+
+    private InterceptorManager interceptorManager;
+    private MessageRouter messageRouter;
+
+    public otrInterceptorPlugin() {
+        interceptorManager = InterceptorManager.getInstance();
+        messageRouter = XMPPServer.getInstance().getMessageRouter();
+    }
 
     @Override
     public void initializePlugin(PluginManager manager, File pluginDirectory) {
-        System.out.println("Hallo Welt");
+        System.out.println("Hallo Weltli");
+        interceptorManager.addInterceptor(this);
     }
 
     @Override
     public void destroyPlugin() {
         System.out.println("Tschuess Welt");
+    }
+
+    @Override
+    public void interceptPacket(Packet packet, Session session, boolean incoming, boolean processed) throws PacketRejectedException {
+
     }
 }
